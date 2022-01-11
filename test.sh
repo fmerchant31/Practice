381861480ba82548b0e2775c72c25906ec8196d5
Fatema <fatemamerchant31@gmail.com>
2022-01-11 14:16:35 +0530

    initial commit

---
+++ test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# echo "hello world!"
+# echo $BASH
+# echo $BASH_VERSION
+# echo $PWD
+# read -p 'enter name: ' name #take input on same line
+# #read -sp 'password: ' pass #forsilent password
+# echo
+# echo "entered name: $name"
+# echo "enter names:"
+# read -a names
+# echo "Names: ${names[0]}, ${names[1]}"
+# name=ftm
+# val=10+10
+# echo my name is $name and value= $val.
+
+# echo $0 $1 $2 '>echo $0 $1 $2'
+a=ff
+if [ $a == "ff" ]
+then    
+    echo "True"
+fi
